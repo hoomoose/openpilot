dd57c3e351dc2ee6d5e88d535771608806ebaaf7
hoomoose <94947902+hoomoose@users.noreply.github.com>
2023-02-07 22:27:22 -0700

    Stop from Start Fix

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -53,6 +53,7 @@ class CarController:
     self.apply_steer_last = 0
     self.car_fingerprint = CP.carFingerprint
     self.last_button_frame = 0
+    self.resume_count = 0
 
   def update(self, CC, CS):
     actuators = CC.actuators
@@ -137,7 +138,7 @@ class CarController:
           self.accel_last = accel
       else:
         # button presses
-        if (self.frame - self.last_button_frame) * DT_CTRL > 0.25:
+        if (self.frame - self.last_button_frame) * DT_CTRL > 0.05:
           # cruise cancel
           if CC.cruiseControl.cancel:
             if self.CP.flags & HyundaiFlags.CANFD_ALT_BUTTONS:
@@ -145,8 +146,11 @@ class CarController:
               self.last_button_frame = self.frame
             else:
               for _ in range(20):
-                can_sends.append(hyundaicanfd.create_buttons(self.packer, self.CP, CS.buttons_counter+1, Buttons.CANCEL))
-              self.last_button_frame = self.frame
+                can_sends.extend([hyundaicanfd.create_buttons(self.packer, self.CP, CS.buttons_counter+1, Buttons.RES_ACCEL)] * 6)
+                self.resume_count += 1
+                if self.resume_count > 5:
+                  self.last_button_frame = self.frame
+                  self.resume_count = 0
 
           # cruise standstill resume
           elif CC.cruiseControl.resume:
