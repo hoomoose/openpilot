190aa71938a6a792cc215667f35b71d9bfa8a5cd
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-27 16:34:47 -0700

    Higher bitrate uploads
    Added toggle to increase the bitrate for uploads to comma connect

--- common/params.cc
+++ common/params.cc
@@ -280,6 +280,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"HideLeadMarker", PERSISTENT},
     {"HideSpeed", PERSISTENT},
     {"HideSpeedUI", PERSISTENT},
+    {"HigherBitrate", PERSISTENT},
     {"LaneLinesWidth", PERSISTENT},
     {"LateralTune", PERSISTENT},
     {"LeadInfo", PERSISTENT},
